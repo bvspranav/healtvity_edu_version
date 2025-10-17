5de127a08f922f1e03e6708b4d09cd1361433dee
B. Venkata Sai Pranav <146656156+bvspranav@users.noreply.github.com>
2025-10-17 13:26:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
 API_URL = os.getenv("API_URL", "http://localhost:8000")
 HISTORY_FILE = os.getenv("HISTORY_FILE", "history.json")
-USE_OPENAI = os.getenv("USE_OPENAI", "true").lower() in ("1", "true", "yes")
+#USE_OPENAI = os.getenv("USE_OPENAI", "true").lower() in ("1", "true", "yes")
+USE_GEMINI = True
+GEMINI_MODEL = "gemini-1.5-pro"
+
+
